c5433bd58b056fb0e647a4514f28a6303a8a0872
LanceGriffin <38203@ocvts.org>
2026-02-12 13:10:17 +0000

    Coding Challenge One Done

---
+++ LGriffin_CodingChallengeOne.py
@@ -0,0 +1,12 @@
+Name = input("What is your name? ")
+Age = int(input("How old are you? "))
+Height = float(input("What is your height in meters? "))
+print(f"My name is {Name}, I am {Age} years old, and I am {Height} meters tall.")
+
+type(Name)
+type(Age)
+type(Height)
+
+#Q 1: The difference between a int and a float is that an int is a whole number, while a float is a decimal number. Example: 5 is an int, 5.1 is a float.
+#Q 2: Python doesn't require variable declarations, because it is a dynamically typed language.
+#Q 3: If {Age} was a string, it would be enclosed in quotes. 
